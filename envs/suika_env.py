118f0c6bf449f9acf2e8fa0fc821baccb9d2edea
kmsk99 <momoqq@naver.com>
2023-11-01 00:06:21 +0900

    코드 개선 중

--- envs/suika_env.py
+++ envs/suika_env.py
@@ -1,25 +1,48 @@
 import gym
 from selenium import webdriver
 import pyautogui
+import time
+from gym.spaces import Discrete
 
 
 class SuikaEnv(gym.Env):
     def __init__(self):
         super().__init__()
-        # 환경 초기화
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://suikagame.com")
+        time.sleep(5)  # 페이지 로딩 시간을 충분히 기다리도록 변경
+        self.game_board = self.driver.find_element(value="GameCanvas")
+        self.canvas_size = self.game_board.size
+        self.action_space = Discrete(
+            self.canvas_size["width"]
+        )  # x 좌표의 가능한 값: 0 to canvas_size['width']
 
     def step(self, action):
-        # 행동 수행 및 보상 계산
-        pass
+        x = self.game_board.location["x"] + action
+        center_y = self.game_board.location["y"] + self.canvas_size["height"] / 2
+        pyautogui.click(x=x, y=center_y)
+        state = self._get_state()
+        reward = self._get_reward()
+        done = self._is_done()
+        return state, reward, done, {}
 
     def reset(self):
-        # 환경 리셋
-        pass
-
-    def render(self):
-        # 환경 렌더링
-        pass
+        self.driver.refresh()
+        time.sleep(5)  # 페이지 리셋 후 로딩 시간을 충분히 기다리도록 변경
+        return self._get_state()
 
     def close(self):
-        # 환경 종료
+        self.driver.quit()
+
+    def _get_state(self):
+        self.game_board.screenshot("game_board.png")
+        # TODO: 게임 상태를 반환하는 방법을 정의
+        pass
+
+    def _get_reward(self):
+        # TODO: 보상을 계산하고 반환
+        pass
+
+    def _is_done(self):
+        # TODO: 게임이 종료되었는지 확인하고 반환
         pass
